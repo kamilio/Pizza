2b28c6d7477fb63935b2c0d16bcc9631277e2aef
Kamil Jopek <kamil.jopek@gmail.com>
2012-03-09 18:36:24 +0100

    Remove reference - not supported by postgre

--- db/migrate/20120307143210_create_items_counts.rb
+++ db/migrate/20120307143210_create_items_counts.rb
@@ -1,8 +1,8 @@
 class CreateItemsCounts < ActiveRecord::Migration
   def change
     create_table :items_counts do |t|
-      t.references :order
-      t.references :item
+      t.integer :order_id
+      t.integer :item_id
       t.integer :count, :default => 1
 
       t.timestamps
