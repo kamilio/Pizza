334192ecdf85874077bc3fa30ff141b5a82ef9a1
Kamil Jopek <kamil.jopek@gmail.com>
2012-03-09 18:29:38 +0100

    Dont compile assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Pizza::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  # dont compile assets
+  config.assets.compile = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
